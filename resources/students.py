789eb23077b08557b68a0cc7378d3216aa278d61
sahilmahendrakar <sahilm020@gmail.com>
2023-09-15 23:07:40 -0400

    task 1

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/sahilmahendrakar/dev/school/cloud-computing/e6156-microservice-1/resources/old-students.json"
+        "/Users/sahilmahendrakar/dev/school/cloud-computing/e6156-microservice-1-sm4967/resources/old-students.json"
 
     def __init__(self):
         self.students = None
